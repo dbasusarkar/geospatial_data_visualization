75c4095d36b8be0fba7a693c8e8a5c218a705e74
dbasusarkar <12450334+dbasusarkar@users.noreply.github.com>
2024-06-22 07:25:46 -0600

    updated

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-# geospatial_data_visualization
+# Geospatial Data Visualization Project
+---
+
+<br/>
+! []  (./world_map.boundary.plot.png)
+
+<br/>
+! []  (./world_map.boundary.png)
+
+<br/>
+! [] (./world_map.plot.png)
+
---
+++ geospatial_visualization.py
@@ -0,0 +1,36 @@
+import pandas as pd
+import geopandas as gpd
+import geoplot as gplt
+import folium
+import shapely
+import contextily as ctx
+from contextily import Place
+import xyzservices.providers as xyz
+import mapclassify as mc
+import matplotlib.pyplot as plt
+import numpy as np
+import math
+
+world_map = gpd.read_file(gplt.datasets.get_path('world'))
+
+world_map.boundary.plot(edgecolor = 'black')
+plt.ylabel("Longitude in degrees (" + u"\u00B0" + ")")
+plt.xlabel("Latitude in degrees (" + u"\u00B0" + ")")
+plt.savefig('world_map.boundary.pdf')
+plt.savefig('world_map.boundary.png')
+
+world_map.plot(cmap = 'viridis', figsize = (16, 10))
+plt.ylabel("Longitude in degrees (" + u"\u00B0" + ")")
+plt.xlabel("Latitude in degrees (" + u"\u00B0" + ")")
+plt.savefig('world_map.plot.pdf')
+plt.savefig('world_map.plot.png')
+
+ax = world_map.plot(cmap='viridis', figsize=(16, 10))
+world_map.boundary.plot(ax=ax, edgecolor='black')
+plt.ylabel("Longitude in degrees (" + u"\u00B0" + ")")
+plt.xlabel("Latitude in degrees (" + u"\u00B0" + ")")
+plt.savefig('./world_map.boundary.plot.pdf')
+plt.savefig('./world_map.boundary.plot.png')
+
+
+
